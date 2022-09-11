0e5cd16b28897d35eed7b45de3b39dd2fed4af95
ping <ping@users.noreply.github.com>
2022-09-11 18:26:55 +0800

    Test tweaks

--- .github/workflows/install_calibre.sh
+++ .github/workflows/install_calibre.sh
@@ -19,9 +19,8 @@ else
   dl_url="https://github.com/kovidgoyal/calibre/releases/download/${tag}/calibre-${latest_version}-${platform}.txz" && \
   sig_url="https://calibre-ebook.com/signatures/calibre-${latest_version}-${platform}.txz.sha512" && \
   sig2_url="https://code.calibre-ebook.com/signatures/calibre-${latest_version}-${platform}.txz.sha512" && \
-  echo "Downloading sig $sig_url ..." && \
-  { curl -L --retry 2 --show-error --silent --fail --output "${bin_folder}/${sig_file}" "$sig_url" || \
-    curl -L --retry 2 --show-error --insecure --fail --silent --output "${bin_folder}/${sig_file}" "$sig2_url"; } && \
+  { echo "Downloading sig $sig_url ..." && curl -L --retry 2 --show-error --silent --fail --output "${bin_folder}/${sig_file}" "$sig_url" || \
+    echo "Downloading sig $sig2_url ..." && curl -L --retry 2 --show-error --insecure --fail --silent --output "${bin_folder}/${sig_file}" "$sig2_url"; } && \
   echo "Downloading bin $dl_url ..." && \
   curl -L --retry 2 --show-error --silent --output "${bin_folder}/${bin_file}.part" "$dl_url" && \
   echo "$(cat "${bin_folder}/${sig_file}")  ${bin_folder}/${bin_file}.part" | sha512sum --check --status && \
